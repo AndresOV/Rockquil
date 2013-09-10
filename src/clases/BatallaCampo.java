ac855f132499e3681cdd074faa0658dc19fb7c73
Carlos Ortega <Carlos Ortega@familiaOrtega>
2013-09-10 13:39:32 -0500

    segundo avance

--- src/clases/BatallaCampo.java
+++ src/clases/BatallaCampo.java
@@ -16,6 +16,7 @@ public class BatallaCampo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // NUEVA LINEA ANDRES 13:38
         VntBienvenido vb =new VntBienvenido();
         vb.setVisible(true);
     }
